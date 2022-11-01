4ab9fb2c56873b28a363e86e974a306a9275d0ba
ConvexHull89 <riosheren89@gmail.com>
2022-11-01 14:06:50 +0700

    add login,register,aboutus,cp page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('/cp', function () {
+    return view('cpdetailpage');
+});
+
