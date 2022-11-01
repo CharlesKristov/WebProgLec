be1bf04b6e0016aaf5f9ff6376fae6ce4a1fa2b1
ashield <xashield@gmail.com>
2022-11-01 23:15:10 +0700

    temporary cp detail page

--- resources/views/cpdetailpage.blade.php
+++ resources/views/cpdetailpage.blade.php
@@ -3,10 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Competitive Programming</title>
 </head>
 <body>
-    ini cp detail page
+        <div class="container">
+        <h1 class = "text-center">Competitive Programming</h1>
+            <div class="container" style ="background-color:white">
+                <h3>DESKRIPSI</h3>
+                <hr style ="color:gray">
+                <p style ="color:gray">Lomba pemrograman menguji kemampuan dan nalar 
+                    peserta dalam menyelesaikan program komputer untuk 
+                    memecahkan permasalahan yang diberikan. Kriteria penilaian 
+                    mencakup kecepatan penulisan program dan ketepatan/efisiensi 
+                    dari program yang dibuat untuk setiap kasus permasalahan terkait.</p>
+                <p style = "color:gray;">
+                Saat pertandingan, peserta akan diberikan deskripsi-deskripsi sejumlah permasalahan dan dalam kurun waktu 3-5 jam peserta harus menyusun dan mengumpulkan sebanyak mungkin program yang dapat menjawab masing-masing permasalahan tersebut.
+                </p>
+                <p style = "color:gray;">
+                Setiap program yang dibuat selain dapat menjawab dengan tepat kasus permasalahan yang diberikan, juga harus dapat dijalankan pada setiap kasus dalam waktu yang amat terbatas. Sehingga, selain peserta diadu dalam kecepatan penulisan program, peserta juga dituntut menemukan/menggunakan algoritma (dan struktur data) yang tepat dan efisien. Bahasa pemrograman yang digunakan antara lain: C, C++, dan Java.
+                </p style = "color:gray;">
+            </div>
+        </div>
 </body>
 </html>
