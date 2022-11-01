87c8c5a9568768bdb17b1e074ea2ea13543466ef
Hunkihas <84310968+Hunkihas@users.noreply.github.com>
2022-11-01 22:08:06 +0700

    Login Page Improvement (#1)
    * Login Page Improvement
    
    * Change Logo Color

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,89 +10,62 @@
   <title>Document</title>
 </head>
 <body class="background-radial-gradient overflow-hidden">
-  
+
   <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
     <div class="row gx-lg-5 align-items-center mb-5">
       <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-        <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
-          The best offer <br />
-          <span style="color: hsl(218, 81%, 75%)">for your business</span>
-        </h1>
-        <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Temporibus, expedita iusto veniam atque, magni tempora mollitia
-          dolorum consequatur nulla, neque debitis eos reprehenderit quasi
-          ab ipsum nisi dolorem modi. Quos?
-        </p>
+        <div class="logoimage">
+            <img src="{{asset('images/Logo.png')}}" alt="" class="Logo">
+        </div>
       </div>
-      <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+      <div class="col-lg-6 mb-5 mb-lg-0 position-relative" id="circleandform">
         <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
         <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
-
         <div class="card bg-glass">
           <div class="card-body px-4 py-5 px-md-5">
+
             <form action="">
-            
+                <h1 class="logintitle"> Login Now </h1>
               <!-- 2 column grid layout with text inputs for the first and last names -->
-              <div class="row">
-                <div class="col-md-6 mb-4">
-                  <div class="form-outline">
-                    <input type="text" id="form3Example1" class="form-control" />
-                    <label class="form-label" for="form3Example1">First name</label>
-                  </div>
-                </div>
-                <div class="col-md-6 mb-4">
-                  <div class="form-outline">
-                    <input type="text" id="form3Example2" class="form-control" />
-                    <label class="form-label" for="form3Example2">Last name</label>
-                  </div>
-                </div>
-              </div>
 
               <!-- Email input -->
               <div class="form-outline mb-4">
-                <input type="email" id="form3Example3" class="form-control" />
+                <input type="email" id="form3Example3" class="form-control" placeholder="Enter email address"/>
                 <label class="form-label" for="form3Example3">Email address</label>
               </div>
 
               <!-- Password input -->
               <div class="form-outline mb-4">
-                <input type="password" id="form3Example4" class="form-control" />
+                <input type="password" id="form3Example4" class="form-control" placeholder="Enter password"/>
                 <label class="form-label" for="form3Example4">Password</label>
               </div>
 
               <!-- Checkbox -->
-              <div class="form-check d-flex justify-content-center mb-4">
+              <div class="form-check">
                 <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
-                <label class="form-check-label" for="form2Example33">
-                  Subscribe to our newsletter
-                </label>
+                <div class="form-check-text">
+                    <label class="form-check-label" for="form2Example33">
+                        Remember me
+                      </label>
+                </div>
               </div>
 
               <!-- Submit button -->
-              <button type="submit" class="btn btn-primary btn-block mb-4">
-                Sign up
-              </button>
-
-              <!-- Register buttons -->
-              <div class="text-center">
-                <p>or sign up with:</p>
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-facebook-f"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-google"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-twitter"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                  <i class="fab fa-github"></i>
-                </button>
+              <div class="loginbtn">
+                <button type="submit" class="btn btn-primary btn-block mb-4">
+                    Login
+                  </button>
               </div>
+
+              <div class="forgotandregis">
+                <a href="" class="forgot">
+                    forgot password?
+                </a>
+                <a href="" class="Regis">
+                    Don't have an account?
+                </a>
+              </div>
+              <!-- Register buttons -->
             </form>
           </div>
         </div>
