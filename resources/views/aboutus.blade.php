5585538e0604992799b44b432f6ac1477b50f3b4
CharlesKristov <charlesdarmawan1@gmail.com>
2022-11-02 11:40:34 +0700

    add aboutUs Page

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>About Us | Technostalgia</title>
 </head>
@@ -25,6 +26,8 @@
             background-position: center;
         "
         >
+        <div class="titleQ">
+            <h1>Want to know about us?</h1>
             <div class="contentTitle">
                 <div class="aboutUsTitle">
                     <h3>Technostalgia</h3>
@@ -33,6 +36,7 @@
                     <p>Technostalgia Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis deserunt molestias aliquam aspernatur nisi? Officia, laborum? Voluptatibus cupiditate officiis placeat eligendi natus velit. Molestiae, rerum ipsa. Repudiandae vel blanditiis non.</p>
                 </div>
             </div>
+        </div>
 
             
         </div>
@@ -41,33 +45,93 @@
     <div class="contentBody">
         <div class="containerLastEvent">
             <div class="lastEventTitle">
-                <h3>Our Last Event</h3>
+                <h2>Our Last Event</h2>
             </div>
+            <hr style="border:1px outset;background-color:#000000;color:#000000;height:5px;width:50%" />
             <div class="lastEventList">
-                <div class="event">
+                <div class="event1">
                     <div class="eventImg">
                         <img src='../images/High-school-comp-2015-MED.jpg' alt="">
                     </div>
-                    <p>Competitive Programming 21</p>
+                    <div class="eventDesc">
+                        <h5>Competitive Programming 21</h5>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, assumenda harum hic molestias quidem ex nisi corporis repellat optio perferendis atque aliquam! Omnis quod facere nobis blanditiis sed. Quasi, odit.</p>
+                    </div>
                 </div>
-                <div class="event">
+                <div class="event2">
                     <div class="eventImg">
                         <img src='../images/Mercer-Programming-Competition.jpg' alt="">
                     </div>
-                    <p>Hackathon Marathon 22</p>
+                    <div class="eventDesc">
+                        <h5>Hackathon Marathon 22</h5>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, assumenda harum hic molestias quidem ex nisi corporis repellat optio perferendis atque aliquam! Omnis quod facere nobis blanditiis sed. Quasi, odit.</p>
+                    </div>
                 </div>
-                <div class="event">
+                <div class="event1">
                     <div class="eventImg">
                         <img src='../images/UWF-ACM-Programming-Competition-.jpg' alt="">
                     </div>
-                    <p>Capture The Flag 25</p>
+                    <div class="eventDesc">
+                        <h5>Capture The Flag 25</h5>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, assumenda harum hic molestias quidem ex nisi corporis repellat optio perferendis atque aliquam! Omnis quod facere nobis blanditiis sed. Quasi, odit.</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="contactUsContainer">
+
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="well well-sm">
+                        <form class="form-horizontal" action="" method="post">
+                            <fieldset>
+                                <legend class="text-center"><h1>Any Question? Contact us!</h1></legend>
+                                
+                                <!-- Name input-->
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label" for="name">Name</label>
+                                    <div class="col-md-9">
+                                        <input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+                                    </div>
+                                </div>
+        
+                <!-- Email input-->
+                <div class="form-group">
+                    <label class="col-md-3 control-label" for="email">Your E-mail</label>
+                    <div class="col-md-9">
+                        <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+                    </div>
+                </div>
+                
+                <!-- Message body -->
+                <div class="form-group">
+                    <label class="col-md-3 control-label" for="message">Your message</label>
+                    <div class="col-md-9">
+                        <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+                    </div>
+                </div>
+                
+                <!-- Form actions -->
+                <div class="form-group">
+                    <div class="col-md-12 text-right">
+                        <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+          </div>
+        </div>
+    </div>
+    
+</div>
     
     <div class="footer">
-
+        
     </div>
 
 </body>
