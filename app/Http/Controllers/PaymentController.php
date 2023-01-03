e5fae76ba72cf1dae0cd83e480a46bfd46a07829
Hunkihas <ard00243@gmail.com>
2023-01-03 08:50:03 +0700

    benerin payment storage

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Models\leader;
 
 class PaymentController extends Controller
@@ -19,18 +20,15 @@ class PaymentController extends Controller
         ]);
 
         $receipt = $request->file('receipt');
-        $receipt->store('public');
+        $receiptName = $receipt->getClientOriginalName();
+        Storage::putFileAs('public/Receipt', $receipt, $receiptName);
 
         // CRUD
         $value = $request->session()->get('leaders');
         $team_name = $value->Team_Name;
         DB::table('leaders')
               ->where('Team_Name' ,$team_name)
-              ->update(['Payment_Photo' => $receipt]);
-
-        DB::table('leaders')
-              ->where('Team_Name' ,$team_name)
-              ->update(['Payment_Status' => 'unverified']);
+              ->update(['Payment_Photo' => $receiptName, 'Payment_Status' => 'unverified']);
 
         return redirect()->to('dashboard')->send();
     }
