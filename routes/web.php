2b904a21ba2ff30bff59af7d8efc83f48807f7c7
ashield <xashield@gmail.com>
2023-01-18 18:17:49 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardAdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
